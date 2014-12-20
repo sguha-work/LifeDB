0e76675ab4680cf6e5645df1936628a81f2256f9
sguha-work <sguha.office@gmail.com>
2014-12-20 20:38:25 +0530

    testing

--- test.php
+++ test.php
@@ -3,11 +3,12 @@
 ?>
 
 <?php
-  	$instance = new LifeDB("fiddleData_new.js");
+	$instance = new LifeDB("fiddleData_new.js");
   	//echo $instance->find("FiddleToCategory", "*", "");
   	//echo $instance->find("FiddleToCategory", "[\"fiddle_id\"]", "");
   	//echo $instance->find("FiddleToCategory", "[\"fiddle_id\"]", "[\"category_id @eq 1\", \"category_id @eq 13\"]", 0, 2);
   	//echo $instance->find("FiddleToCategory", "[\"fiddle_id\",\"fiddlex_id\"]", "category_id @eq 1 && fiddle_id @ne 2");
-  	echo $instance->find("FiddleToCategory","[\"fiddle_id\",\"category_id\"]","category_id @eq 17");
+  	//echo $instance->find("FiddleToCategory","[\"fiddle_id\",\"category_id\"]","category_id @eq 17");
   	//echo $instance->update("FiddlesData","fiddle_thumb", "x.png", "fiddle_id @le 4");
+  	echo $instance->insert("Student","[{\"name\":\"angshu\",\"age\":\"26\"},{\"name\":\"piklu\",\"age\":\"27\"}]");
 ?>
