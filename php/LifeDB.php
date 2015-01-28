267a30a9665a80cc0564e4fddedd2094db6cd747
Sahasrangshu Guha <sguha1988.life@gmail.com>
2015-01-28 12:14:41 +0530

    Update LifeDB.php

--- php/LifeDB.php
+++ php/LifeDB.php
@@ -14,7 +14,7 @@
 			}
 		}
 		// public functions accessible by users
-		public function insert($pageName, $record) { // insert a record under specified page
+		public function insert($pageName, $record) { // insert a record under specified page return 1 or 0
 			return $this->initiateInsertProcess($pageName, $record);
 		}
 		public function destroy($willDeleteFileAlso = false) { // destroy the whole database
@@ -45,6 +45,7 @@
 						$contentFilteredByQuery = $contentOfFile[$pageName];
 					}
 					$resultSet = $this->matchAndDeleteContent($contentOfFile, $contentFilteredByQuery);// deleting the elements which will be updated and reinserted 
+					return $this->writeJsonStringToFile(json_encode($resultSet));
 				} else { // if attribute name is provided then only the attributes will be removed along with value
 
 				}
@@ -88,37 +89,40 @@
 				foreach($updatedResultSet as $record) {// inserting the updated data to database
 					array_push($resultSetWithoutMatchedContent[$pageName], $record);				
 				}
-				$this->writeJsonStringToFile(json_encode($resultSetWithoutMatchedContent));// writing the uodate to database
-				return $updatedResultSet;
+				return $this->writeJsonStringToFile(json_encode($resultSetWithoutMatchedContent));// writing the uodate to database
 			}
 		}
 		private function updateContent($contentFilteredByQuery, $attributeName, $newValue) {
-			$jsonContent = json_encode($contentFilteredByQuery);
+			$attributeName = json_decode($attributeName, true);
+			$newValue      = json_decode($newValue, true);
+			$jsonContent   = json_encode($contentFilteredByQuery);
 			$contentFilteredByQuery = NULL;
 			$updatedContent = "";
-			for($index=0; $index<strlen($jsonContent); $index++) {
-				if($jsonContent[$index]==$attributeName[0]) {
-					$flag = 0;
-					for($tempIndex=0; $tempIndex<strlen($attributeName); $tempIndex++) {
-						if($jsonContent[$index+$tempIndex]!=$attributeName[$tempIndex]) {
-							$flag = 1;
-							break;
+			for($atValIndex = 0; $atValIndex<count($attributeName); $atValIndex++) {
+				for($index=0; $index<strlen($jsonContent); $index++) {
+					if($jsonContent[$index]==$attributeName[$atValIndex][0]) {
+						$flag = 0;
+						for($tempIndex=0; $tempIndex<strlen($attributeName[$atValIndex]); $tempIndex++) {
+							if($jsonContent[$index+$tempIndex]!=$attributeName[$atValIndex][$tempIndex]) {
+								$flag = 1;
+								break;
+							}
 						}
-					}
-					if($flag == 0) {// attribute value needs to be updated
-						$updatedContent .= $attributeName . "\":" . '"'.$newValue.'"';
-						$tempIndex = $index;
-						while($jsonContent[$tempIndex]!=','&&$jsonContent[$tempIndex]!='}'&&$jsonContent[$tempIndex]!='') {
-							$tempIndex+=1;
+						if($flag == 0) {// attribute value needs to be updated
+							$updatedContent .= $attributeName[$atValIndex] . "\":" . '"'.$newValue[$atValIndex].'"';
+							$tempIndex = $index;
+							while($jsonContent[$tempIndex]!=','&&$jsonContent[$tempIndex]!='}'&&$jsonContent[$tempIndex]!='') {
+								$tempIndex+=1;
+							}
+							$index = $tempIndex - 1;
+						} else {
+							$updatedContent .= $jsonContent[$index];
 						}
-						$index = $tempIndex - 1;
 					} else {
 						$updatedContent .= $jsonContent[$index];
 					}
-				} else {
-					$updatedContent .= $jsonContent[$index];
+					
 				}
-				
 			}
 			return json_decode($updatedContent, true);
 		}
