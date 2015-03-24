639200590cd9890ec52fabc3995f996b9e488277
unknown <sahasrangshu.guha@Fusionbox031.Fusionator.local>
2015-03-24 16:50:45 +0530

    argument value type changes in fetchFromCache and writeToCache

--- test.php
+++ test.php
@@ -22,6 +22,6 @@
 
     //echo $instance->delete('FiddlesData', '*', "fiddle_id @eq :1");
     //echo $instance->find("ChildCategoryData", "[\"cat_id\"]","cat_name @eq :Chart");
-  //echo $instance->find("FiddlesData", "[\"fiddle_id\"]");
-  echo $instance->getPages(3,0);
+  echo $instance->find("FiddlesData", "[\"fiddle_id\"]");
+  //echo $instance->getPages(3,0);
 ?>
